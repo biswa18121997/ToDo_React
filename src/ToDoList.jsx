824c8792a41feec6da895b6b4769eeca9601996e
biswa18121997 <biswajitshrm6@gmail.com>
2025-04-03 15:37:16 +0530

    finally done

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -9,7 +9,7 @@ function ToDoList({todoList,deleteTask,toggleComp,edit,comp }){
     
         
 
-    return (<div className="grid lg:grid-cols-3 gap-2 sm:grid-cols-1 md:grid-cols-2 z-20 ">
+    return (<div className="grid lg:grid-cols-3 gap-2 sm:grid-cols-1 md:grid-cols-2 z-20  p-4 overflow-hidden">
         {todoList.map((tasks)=>(<ToDoItem key={tasks.id} title={tasks.Title} task={tasks.Task} time={tasks.TaskDate}  deleteTask={()=>deleteTask(tasks.id)} toggleComp={()=>toggleComp(tasks.id)} edit={()=>edit(tasks.id)} comp={tasks.Completed}/>))}
     </div>);
 }
